5d1d33f9d9b3645a089f6e164a50d0fa7a02c1e1
Ashwin0006 <ashcrafts0006@gmail.com>
2025-01-18 17:32:28 +0530

    loading screen and text changes

---
+++ loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 4000);
+});
